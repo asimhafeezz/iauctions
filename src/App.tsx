4f2cde1aa702d56cc17ecba6a81dbdfdca07006a
Afraz Ahmad <afraz@arthur.digital>
2021-04-01 14:13:24 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import "./styles/main.scss"
+import { useEffect } from "react";
+import axios from "axios";
+
+import "./styles/main.scss";
 
 import {
-	BrowserRouter as Router,
-	Route,
-	Switch,
-	Redirect,
-} from "react-router-dom"
-import { AddBid, Detail, Listings, Login, Signup } from "./components"
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import {
+  AddBid,
+  Detail,
+  Listings,
+  Login,
+  Signup,
+  AddListing,
+} from "./components";
 
 const App: React.FC = () => {
-	return (
-		<Router>
-			<Switch>
-				<Redirect exact from='/' to='/login' />
-				<Route exact path='/login' component={Login} />
-				<Route exact path='/signup' component={Signup} />
-				<Route exact path='/listings' component={Listings} />
-				<Route exact path='/detail/:id' component={Detail} />
-				<Route exact path='/addbid' component={AddBid} />
-			</Switch>
-		</Router>
-	)
-}
+  return (
+    <Router>
+      <Switch>
+        <Redirect exact from="/" to="/login" />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/listings" component={Listings} />
+        <Route exact path="/detail/:id" component={Detail} />
+        <Route exact path="/addListing" component={AddListing} />
+        <Route exact path="/addbid/:listingId" component={AddBid} />
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/components/detail.tsx
+++ src/components/detail.tsx
@@ -1,30 +1,57 @@
+import { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import axios from "../utils/axios";
+import { listingsUrl } from "../utils/utils";
+
 const Comments = () => (
-	<section className='comment'>
-		<p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-	</section>
-)
-export const Detail: React.FC = () => {
-	return (
-		<div className='detail'>
-			<h1>Bid Detail</h1>
-			<section className='listing-detail'>
-				<h2>Title</h2>
-				<p>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam
-					quibusdam eveniet non odio soluta sequi mollitia temporibus, ipsum
-					quidem velit pariatur molestias, quo ab eius omnis? Animi laborum
-					veritatis sequi.
-				</p>
-				<h6>Price: {`1500$`}</h6>
-			</section>
-			<h2>Comments</h2>
-			<section className='comments'>
-				{Array(5)
-					.fill(0)
-					.map(_ => (
-						<Comments />
-					))}
-			</section>
-		</div>
-	)
+  <section className="comment">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+  </section>
+);
+
+interface Listing {
+  title: string;
+  price: number;
+  description: string;
+  createdAt: string;
 }
+
+export const Detail: React.FC = () => {
+  let params = useParams();
+  const { push } = useHistory();
+  const [listing, setListing] = useState<Listing | null>(null);
+
+  let { id }: any = params;
+  useEffect(() => {
+    const apiUrl = `${listingsUrl}/${id}`;
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setListing(response.data.data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }, [id]);
+  return (
+    <div className="detail">
+      <h1>Lisitng Detail</h1>{" "}
+      <section className="right-side-btn">
+        <button onClick={() => push(`/addbid/${id}`)}>Add New Bid</button>
+      </section>
+      <section className="listing-detail">
+        <h2>Title: {listing?.title}</h2>
+        <p>Description: {listing?.description}</p>
+        <h6>Price: {listing?.price}</h6>
+      </section>
+      <h2>Comments</h2>
+      <section className="comments">
+        {Array(5)
+          .fill(0)
+          .map((_) => (
+            <Comments />
+          ))}
+      </section>
+    </div>
+  );
+};
